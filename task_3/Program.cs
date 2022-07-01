69dc90389902e8c5c6f1331d90030be5169d4914
nikolaev mikhail <6n2n69@gmail.com>
2022-07-02 01:36:53 +0300

    task 3 add

---
+++ task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double count = 0;
+while (count<=N-1)
+{
+    count +=1;
+    Console.WriteLine(count*count*count);
+}
