7991a7947842fd7d7cecf771243c82d80994d81d
nikolaev mikhail <6n2n69@gmail.com>
2022-07-02 00:43:50 +0300

    add gitignore and task_1 edited

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.WriteLine("enter a five-digit number: ");
+string number = Console.ReadLine();
+if (number.Length == 5 && number[0]==number[4] && number[1]==number[3])
+{
+    Console.WriteLine("the number is a palindrome");
+}
+else
+{
+    Console.WriteLine("number is not a palindrome");
+}
